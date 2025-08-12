e7122506cc71ce0ed4fcedfd9154a182dde2211d
ZOOSDOBOOB\26834 <2683459962@qq.com>
2025-08-12 14:40:17 +0800

    Markdown中Ketex数学公式渲染

--- src/components/MarkdownRenderer.vue
+++ src/components/MarkdownRenderer.vue
@@ -1,21 +1,38 @@
 <script setup>
 import MarkdownIt from 'markdown-it'
-import markdownItKatex from 'markdown-it-katex'
+import texmath from 'markdown-it-texmath'
+import katex from 'katex'
 import 'katex/dist/katex.min.css'
+import 'markdown-it-texmath/css/texmath.css'
+
+const props = defineProps({
+  content: String
+})
 
 const md = new MarkdownIt({
   html: false,
   linkify: true,
   breaks: true
-}).use(markdownItKatex)
+}).use(texmath, {
+    engine: katex,
+    delimiters: 'dollars',
+  })
+  .use(texmath, {
+    engine: katex,
+    delimiters: 'brackets',
+  //   katexOptions: {
+  //     strict: false,    // 允许未知字符
+  //     trust: true,      // 允许特殊命令
+  //     fleqn: true       // 可选，左对齐
+  // }
+  })
 
-defineProps({
-  content: String
-})
+import { computed } from 'vue'
+const rendered = computed(() => md.render(props.content || ''))
 </script>
 
 <template>
-  <div class="markdown-body" v-html="md.render(content || '')"></div>
+  <div class="markdown-body" v-html="rendered"></div>
 </template>
 
 <style src="github-markdown-css/github-markdown-light.css"></style>
